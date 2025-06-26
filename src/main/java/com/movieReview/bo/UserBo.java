43ed7aa33468755b1e7aed440841dac849c57d71
Chandrakanth <ckanth655@gmail.com>
2025-06-26 13:59:00 +0530

    first commit

---
+++ src/main/java/com/movieReview/bo/UserBo.java
@@ -0,0 +1,14 @@
+package com.movieReview.bo;
+
+import lombok.Data;
+
+@Data
+public class UserBo {
+
+	private String firstName;
+	private String password;
+	private String email;
+	private String lastName;
+	private String phoneNumber;
+
+}
